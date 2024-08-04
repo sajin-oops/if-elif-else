fea5b00e771a325e0cb6053ba83b00b0670487a6
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-04 15:46:39 +0530

    Add files via upload

---
+++ if-elif-else.py
@@ -0,0 +1,11 @@
+letter = "A"
+if letter == "B":
+  print("letter is B")
+elif letter == "C":
+  print("letter is C")
+elif letter == "A":
+  print("letter is A")
+else:
+  print("letter isn't A,B and C")
+    
+#O/P -  letter is A
